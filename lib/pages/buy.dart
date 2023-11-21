04368ae0450eb1aaaf9fb6299fd3f516af7d5401
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-11-20 22:46:26 -0500

    updated styles

--- lib/pages/buy.dart
+++ lib/pages/buy.dart
@@ -5,15 +5,35 @@ class Buy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(15),
+          padding: const EdgeInsets.all(15),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                'Buy page',
+              const Text(
+                'Enter the amount ot buy',
               ),
+              TextFormField(
+                showCursor: false,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 70, color: Colors.amberAccent),
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "0",
+                ),
+              ),
+              const Text(
+                'approx. 1,200 USD',
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Buy"),
+              )
             ],
           ),
         ),
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,41 +16,27 @@ class Home extends StatelessWidget {
           child: Center(
             child: Column(
               children: [
-                Text(
-                  parsedDate,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    // fontSize: 50,
-                    color: Colors.amber,
-                  ),
-                ),
-                const Text(
-                  'BTC/USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 50,
-                    color: Colors.amber,
-                  ),
-                ),
                 Card(
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
                   margin: const EdgeInsets.all(15),
                   elevation: 10,
-                  child: const Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      children: [
+                        Text(parsedDate),
+                        const Text(
                           "35.5K",
                           style: TextStyle(
-                            fontSize: 50,
+                            fontSize: 70,
                             color: Colors.amber,
                           ),
                         ),
-                      ),
-                    ],
+                        const Text('BTC/USD')
+                      ],
+                    ),
                   ),
                 )
               ],
@@ -61,3 +47,18 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+
+// const Column(
+//                     children: [
+//                       Padding(
+//                         padding: EdgeInsets.all(20),
+//                         child: Text(
+//                           "35.5K",
+//                           style: TextStyle(
+//                             fontSize: 50,
+//                             color: Colors.amber,
+//                           ),
+//                         ),
+//                       ),
+//                     ],
