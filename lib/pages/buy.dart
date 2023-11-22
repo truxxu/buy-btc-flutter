7bd838ff8fb140286876de190da15da89df9f4da
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-11-21 22:04:24 -0500

    validates form

--- lib/pages/buy.dart
+++ lib/pages/buy.dart
@@ -5,38 +5,96 @@ class Buy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Enter the amount ot buy',
-              ),
-              TextFormField(
-                showCursor: false,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 70, color: Colors.amberAccent),
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "0",
-                ),
-              ),
-              const Text(
-                'approx. 1,200 USD',
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Buy"),
-              )
-            ],
-          ),
+          padding: EdgeInsets.all(15),
+          child: BuyForm(),
         ),
+      ),
+    );
+  }
+}
+
+class BuyForm extends StatefulWidget {
+  const BuyForm({
+    super.key,
+  });
+
+  @override
+  State<BuyForm> createState() => _BuyFormState();
+}
+
+class _BuyFormState extends State<BuyForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _validationError;
+
+  bool get _showError => _validationError != null;
+
+  String? Function(String?)? get validator => (value) {
+        if (value == null || value.isEmpty) {
+          return 'error';
+        }
+        return null;
+      };
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Enter the amount ot buy',
+          ),
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            keyboardType: TextInputType.number,
+            validator: (value) {
+              final error = validator?.call(value);
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                setState(() {
+                  _validationError = error;
+                });
+              });
+              return null;
+            },
+            showCursor: false,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 70,
+              color: Colors.amberAccent,
+            ),
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintText: "0",
+            ),
+            textInputAction: TextInputAction.next,
+          ),
+          const Text(
+            '~ 1,200 USD',
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          if (_showError)
+            const Text(
+              "Please enter a valid amount",
+              style: TextStyle(
+                color: Colors.red,
+              ),
+            ),
+          ElevatedButton(
+            onPressed: () {
+              if (!_formKey.currentState!.validate()) {
+                debugPrint('success!');
+              }
+            },
+            child: const Text("Buy"),
+          ),
+        ],
       ),
     );
   }
